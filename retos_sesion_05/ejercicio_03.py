1b2dbf368ba773deb51d9ffa9b1aa7888f393ae1
Ale Torrez <ar.torrez@acad.ucb.edu.bo>
2024-05-01 16:47:03 -0400

    Ejercicio 3 reto 5

---
+++ retos_sesion_05/ejercicio_03.py
@@ -0,0 +1,9 @@
+#Convertir a cuantos días, horas, minutos y segundos corresponde la 
+#siguiente cantidad de segundos: 288325
+
+seg = 288325
+min = seg//60
+horas = min//60
+dias = horas//24
+
+print(seg,min,horas,dias)
