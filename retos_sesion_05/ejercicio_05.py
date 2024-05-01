7a41fb735fb3dc9facecfaa27d3bd606618b37e1
Ale Torrez <ar.torrez@acad.ucb.edu.bo>
2024-05-01 16:34:39 -0400

    Ejercicio 1 reto 5

---
+++ retos_sesion_05/ejercicio_05.py
@@ -0,0 +1,7 @@
+#Escribe un programa en Python que calcule el salario si la cantidad de 
+#horas trabajadas es 160 y la tarifa por hora de trabajo es 5.5 USD/hora
+
+horas = 160
+tarifa = 5.5
+salario = horas * tarifa
+print (salario)
