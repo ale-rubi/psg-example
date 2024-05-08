723587634bccd9c434b34df84ccbe3e89d5fba18
Ale Torrez <ar.torrez@acad.ucb.edu.bo>
2024-05-08 19:49:12 -0400

    Retos 06 Ejercicio 5

--- retos_sesion_06/ejercicio_05.py
+++ retos_sesion_06/ejercicio_05.py
@@ -1,6 +1,5 @@
 #Comparar los números 123 y 890, comprobar si tienen la misma paridad ambos pares o ambos impares
-par1 = 123
-par2 = 890
-print(((par1+par2)%2)==0)
+par1 = 12
+par2 = 12
+print(par1%2==0 and par2%2==0)
 
-#Si ambos son pares su suma tambien es par, caso contrario False
