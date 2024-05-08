270889578917ee4a7a1ffa0a09de29f83885a6b9
Ale Torrez <ar.torrez@acad.ucb.edu.bo>
2024-05-08 18:20:32 -0400

    Retos 06 Ejercicio 2

---
+++ retos_sesion_06/ejercicio_02.py
@@ -0,0 +1,12 @@
+a = True
+b = False
+print ((a or b) and not (a and b))
+b = True
+a = False
+print ((a or b) and not (a and b))
+a = True
+b = True
+print ((a or b) and not (a and b))
+a = False
+b = False
+print ((a or b) and not (a and b))
