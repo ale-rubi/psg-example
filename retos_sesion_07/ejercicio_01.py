7e980f23a38855bdb297fbe9fde0355627d2a450
Ale Torrez <ar.torrez@acad.ucb.edu.bo>
2024-05-08 20:34:55 -0400

    Retos 07 Ejercicio 1

---
+++ retos_sesion_07/ejercicio_01.py
@@ -0,0 +1,8 @@
+#Escribir una cadena con una frase de una canción o un poema
+
+song = '''Aime-moi dans la neige
+Aime-moi sous le soleil
+Aime-moi la peau beige
+Dans les fleurs de vermeilles'''
+
+print (song)
