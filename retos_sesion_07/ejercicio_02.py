fbf1b593744e2f81f86659dc5aa17ce4664e7563
Ale Torrez <ar.torrez@acad.ucb.edu.bo>
2024-05-08 20:45:18 -0400

    Retos 07 Ejercicio 2

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,7 @@
+#Escribe un programa que reciba dos enteros por teclado y sume su resultado
+
+entero1 = int(input("Ingrese un valor entero: "))
+entero2 = int(input("Ingrese un valor entero: "))
+suma = entero1+entero2
+
+print (suma)
