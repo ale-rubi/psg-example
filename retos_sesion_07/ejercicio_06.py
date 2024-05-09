1a6c1f2ff55d5b64e9cc0a2f16646bbd3abd11c7
Ale Torrez <ar.torrez@acad.ucb.edu.bo>
2024-05-08 21:41:23 -0400

    Retos 07 Ejercicio 6

---
+++ retos_sesion_07/ejercicio_06.py
@@ -0,0 +1,19 @@
+# Agregar 5 Ejemplos con otras funciones no vistas en la sesión
+
+ejemplo = "La llama que llama"
+
+print("Funcion Index")
+print (ejemplo.index("la"))
+
+print("Funcion RIndex")
+print (ejemplo.rindex("la"))
+
+print("Funcion removeprefix")
+print (ejemplo.removeprefix('La '))
+
+print("Funcion removesuffix")
+print (ejemplo.removesuffix('ma'))
+
+print("Funcion partition")
+print (ejemplo.partition("llama"))
+
